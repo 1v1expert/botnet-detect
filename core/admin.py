8d6a5b90664de79886dd75851cc2c5c706207cc3
Vladislav <1v1expert@gmail.com>
2020-06-01 03:19:18 +0300

    create admin view

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Frame, Node, Vector
+
+
+class FrameAdmin(admin.ModelAdmin):
+    list_display = ['length', 'destination', 'source']
+
+
+# noinspection SpellCheckingInspection
+class VectorAdmin(admin.ModelAdmin):
+    list_display = ['sip', 'dip', 'srcpkts', 'drcpkts']
+
+
+# noinspection SpellCheckingInspection
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'outdegree', 'indegree', 'outgoing_weight', 'incoming_weight', 'betweenness_centrality',
+                    'closeness_centrality', 'eigenvector_centrality', 'clustering_coefficient', 'alpha_centrality']
+
+
+admin.site.register(Frame, FrameAdmin)
+admin.site.register(Vector, VectorAdmin)
+admin.site.register(Node, NodeAdmin)
